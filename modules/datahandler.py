241833a0e1a8a3800b2d7b4d433e0f3f16653dca
Hurairah <abuhurairah5112@gmail.com>
2024-06-17 14:09:25 +0900

    Finial changes before testing start

--- modules/datahandler.py
+++ modules/datahandler.py
@@ -4,21 +4,28 @@ from modules.logger import log
 
 def dataHandler(data):
     if data is not None:
-        cntpos = 0
         action = data.split()
-        trade(action[0])
-        time = log(action[0])
-
         cntpos = 1 if action[0] == 'buy' or action[0] == 'sell' else 0
-
+        
         with open('data/position.json', 'r+') as pos:
             position_data = json.load(pos)
             position = position_data['positions']
             pos.seek(0)
-            if action[0] == 'buy': json.dump({'positions': cntpos + position}, pos, indent=4)
-            if action[0] == 'sell': json.dump({'positions': position - cntpos}, pos, indent=4)
+              
+            if action[0] == 'buy' and position < 1: 
+                trade(action[0])
+            elif action[0] == 'sell' and position > 0:
+                trade(action[0])
+            else:
+                return 'You alr got trades open at this time'
+                
+            time = log(action[0])
+            if action[0] == 'buy': 
+                json.dump({'positions': cntpos + position}, pos, indent=4)
+            if action[0] == 'sell': 
+                json.dump({'positions': position - cntpos}, pos, indent=4)     
+                
             pos.truncate()
-            
-        return 'The trade for ' + action[0] + ' has been executed at ' + time
+            return 'The trade for ' + action[0] + ' has been executed at ' + time
     else:
         pass
