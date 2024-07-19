6892753d69cb5da8d088f3ceb01f1a6874b2f6b8
Hurairah <abuhurairah5112@gmail.com>
2024-07-19 23:08:18 +0900

    just updating pine and some cool stuff

--- test.py
+++ test.py
@@ -1,63 +1,42 @@
-import json
-from modules.trader import trade
-from modules.logger import log
-from datetime import datetime
-from modules.sendMessage import send
+import os
+from time import sleep
+import requests
+os.system('cls')
 
 
-def dataHandler(data):
-    if data is not None:
-        action = data.split()
-        cntpos = 1 if action[0] in ['buy', 'sell'] else 0
-        current_time = datetime.now().strftime("%d %H:%M")
-        
-        try:
-            with open('data/position.json', 'r+') as pos:
-                position_data = json.load(pos)
-                position = position_data['positions']
-            
-                if action[0] == 'buy':
-                    if position == 0:
-                        trade(action[0])
-                        position += cntpos
-                        send(f'The {action[0]} trade has been executed at {current_time}')
+def init(text):
+    print("\n", text , end="", flush=True)
+    for i in range(3):
+        sleep(1)
+        print(".", end="", flush=True)
+def check_internet():
+    
+    url = "http://www.google.com"
+    timeout = 5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        print("\n Connected to the Internet")
+    except requests.ConnectionError:
+        print("No internet connection")
 
-                    elif position < 0:
-                        for x in range(abs(position)):
-                            trade('buy')
-                            position += cntpos
-                        trade(action[0])
-                        position += cntpos
-                        send(f'The {action[0]} trade has been executed at {current_time}')
+sleep(1)
+print("""                                                                                                                          """)
+print("""                                                                                                                          """)
+print("""                                                                                                                          """)
+print("""        ||||||||||| ||||||||||    |||     ||   ||||    || |||||||||||       |||     |||  ||||||||        |||        |||||||""")
+sleep(0.035)
+print("           |+|         |+|      |+|+|   |+|  |+|+|   |+|     |+|           |+|     |+| |+|    |+|     |+|+|       |+|   |+|")
+sleep(0.035)
+print("          +|+         +|+      |+|+|+  +|+  |+|+|+  +|+     +|+           +|+     +|+       +|+        +|+       +|+   +|+")
+sleep(0.025)
+print("         +#+         +#+      +#+ +|+ +#+  +#+ +|+ +#+     +#+           +#+     +|+     +#+          +#+       +#+   +|+")
+sleep(0.025)
+print("        +#+         +#+      +#+  +#+#+#  +#+  +#+#+#     +#+            +#+   +#+    +#+            +#+       +#+   +#+")
+sleep(0.015)
+print("  +#   +#+         #+#      #+#   #+#+#  #+#   #+#+#     #+#             #+#+#+#    #+#       #+#   #+#   #+# #+#   #+#")
+sleep(0.015)
+print(" #######     ###########  ###    ####  ###    #### ###########           ###     ########## ### ####### ###  #######")
 
-                    elif position >= 1:
-                        pass
-
-                elif action[0] == 'sell':
-                    if position == 0:
-                        trade(action[0])
-                        position -= cntpos
-                        send(f'The {action[0]} trade has been executed at {current_time}')
-
-                    elif position > 0:
-                        for x in range(position):
-                            trade('sell')
-                            position -= cntpos
-                        trade(action[0])
-                        position -= cntpos
-                        send(f'The {action[0]} trade has been executed at {current_time}')
-                        
-                    elif position <= -1:
-                        pass
-
-                pos.seek(0)
-                json.dump({'positions': position}, pos, indent=4)
-                pos.truncate()
-
-                log_time = log(action[0])
-                return f'The trade for {action[0]} has been executed at {log_time}'
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            return 'An error occurred while handling the data.'
-    else:
-        pass
+init(text='Starting system')
+init(text="Checking internet connectivity")
+check_internet()
