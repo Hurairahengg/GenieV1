664023b64b7b68d22444ebcc244a62e4118b1e77
Hurairah <abuhurairah5112@gmail.com>
2024-07-22 01:08:24 +0900

    cords update

--- modules/trader.py
+++ modules/trader.py
@@ -4,30 +4,28 @@ from time import sleep
 
 class actions:
     def buy():
-        pyauto.moveTo(-2780, 720, duration=0.2)
+        pyauto.moveTo(-2550, 730, duration=0.2)
         pyauto.click()
         pyauto.hotkey('shift', 'b')
         sleep(0.3)
-        pyauto.moveTo(-2150, 660, duration=0.2)
+        pyauto.moveTo(-2150, 935, duration=0.2)
         pyauto.click(duration=0.2)
 
     def sell():
-        pyauto.moveTo(-2780, 720, duration=0.2)
+        pyauto.moveTo(-2550, 730, duration=0.2)
         pyauto.click()
         pyauto.hotkey('shift', 's')
         sleep(0.3)
-        pyauto.moveTo(-2150, 660, duration=0.2)
+        pyauto.moveTo(-2150, 935, duration=0.2)
         pyauto.click(duration=0.2)
 
 
 def trade(action):
     if action == 'buy':
         actions.buy()
-        return 'ok'
 
     elif action == 'sell':
         actions.sell()
-        return 'ok'
     
     else:
         pass
